6345e69103136e672bc512297fcae54bfa88e07e
Clare Lindley <clare.lindley@and.digital>
2024-04-24 16:04:56 +0100

    WIP

--- index.js
+++ index.js
@@ -7,8 +7,8 @@ app.use(express.json());
 const requestList = new Map()
 
 /**
- * what if GPT sesh has expired?
- * what if we can't find the request id in the list of requests?
+ * @todo what if GPT sesh has expired?
+ * @todo what if we can't find the request id in the list of requests?
  */
 
 app.get("/test", (_, res) => {
@@ -24,11 +24,13 @@ app.post("/incoming", (req, res) => {
     callZapier(requestId, body)
 });
 
+// fire and forget to this webhook. When it's ready it will call our /outgoing endpoint (we need to configure Zapier to do that!)
 function callZapier(requestId, body){
     console.log('calling zapier')
     console.log('requestId ', requestId);
 }
 
+// Zapier is configured to call this with the data we care about
 app.post("/outgoing", (req, res) => {
     const body = req.body
     const requestId = body.requestId // also contains the data we care about
@@ -40,3 +42,5 @@ app.post("/outgoing", (req, res) => {
 })
 
 app.listen(8080)
+
+module.exports = app
