1b0ec269f7d46a1d83cccb7a18e1baa87738c1bc
Clare Lindley <clare.lindley@and.digital>
2024-04-24 15:20:42 +0100

    WIP

---
+++ gpt-spike.js
@@ -0,0 +1,35 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const requestList = new Map()
+
+/**
+ * what if GPT sesh has expired?
+ * what if we can't find the request id in the list of requests?
+ */
+
+app.post("/incoming", (req, res) => {
+    const body = req.body
+    const requestId = Math.ceil(Math.random(0,1) * 10000).toString();
+    requestList.set(requestId,res)
+    callZapier(requestId, body)
+});
+
+function callZapier(requestId, body){
+    console.log('calling zapier')
+    console.log('requestId ', requestId);
+}
+
+app.post("/outgoing", (req, res) => {
+    const body = req.body
+    const requestId = body.requestId // also contains the data we care about
+    const chatGPTResponse = requestList.get(requestId)
+    chatGPTResponse.send({score: '100%'});
+    res.status(200)
+    requestList.delete(requestId)
+})
+
+app.listen(8080)
